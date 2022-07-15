4e087f657e87910a6a63bd1f55e5dc06332ba10a
pyamk <pyamkrymy33@PAYAM>
2022-07-15 10:24:17 +0430

    Contact added

--- DevCart_mvc/Controllers/HomeController.cs
+++ DevCart_mvc/Controllers/HomeController.cs
@@ -1,32 +1,29 @@
 ﻿using DevCart_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace DevCart_mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
