00a4c007d35c1907d7c42ae5a065d126ed85b191
pyamk <pyamkrymy33@PAYAM>
2022-07-17 02:02:33 +0430

    Project section from index page moved to partial view project

---
+++ DevCart_mvc/Models/IndexModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DevCart_mvc.Models
+{
+    public class IndexModel
+    {
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+
+}
---
+++ DevCart_mvc/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace DevCart_mvc.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string client { get; set; }
+    }
+
+}
